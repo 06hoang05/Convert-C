244b1b5dcb7c0da6717047e95ba99a32f9648eb9
06hoang05 <97800448+06hoang05@users.noreply.github.com>
2022-08-18 18:44:24 +0700

    Add files via upload

---
+++ Bubddle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Bubddle
+{
+    public static void Main()
+    {
+        int[] a = { 10, 3, 21, 6, 1 };
+        int t;
+        Console.WriteLine("Bubddle Sort");
+        Console.WriteLine("Mang ban dau");
+        foreach (int arr in a)
+          /* Console.WriteLine(arr + "");*/
+        for (int i = 0; i <= a.Length -2; i++)
+        {
+            for (int j = 0; j <= a.Length-2; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    t = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = t;
+                }
+            }
+        }
+        Console.WriteLine("\nMang da sap xep : ");
+        foreach(int arr in a)
+           /* Console.WriteLine(arr + "");*/
+        
+        Console.ReadKey();
+    }
+}
